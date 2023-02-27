e172674a05223e62a797bace7a40ae9e2ca0ab55
jaymorelli <jean.morelli@colt.net>
2023-02-27 10:17:28 +0000

    added read me - data structures

--- data-structures/src/main/java/io/github/jaymorelli/Main.java
+++ data-structures/src/main/java/io/github/jaymorelli/Main.java
@@ -1,9 +1,10 @@
 package io.github.jaymorelli;
 
-import java.util.LinkedList;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
+        Stack<String> s = new Stack<>();
         System.out.println("Hello Lists!");
     }
 
